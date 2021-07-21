b134801fdb723dee03b09b7499fac54ef9bd7d9f
surya <sprathap049@gmail.com>
2021-07-21 02:13:23 +0000

    3rd commit check once and update

--- hello.java
+++ hello.java
@@ -2,7 +2,8 @@ class Hello
 {
 public static void main(String args[])
 
-System.out.println("some changes have been done");
+System.out.println("make some change sby me 3rd time will check once update if an thing misssing");
+		
 }
 }
 
